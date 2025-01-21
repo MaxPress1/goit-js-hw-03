e21fd2ddb8b519ffbfbb4421cccc992610da9357
MaxPress <maxpress96@ukr.net>
2025-01-21 14:20:37 +0200

    add script

--- js/task-3.js
+++ js/task-3.js
@@ -1,17 +1,16 @@
-function getElementWidth(content, padding, border) {
-    
-    const contentWidth = parseFloat(content); 
-    
-    const paddingWidth = parseFloat(padding); 
+function filterArray(numbers, value) {
+    const result = [];
 
-    const borderWidth = parseFloat(border);
-
-    return contentWidth + (paddingWidth * 2) + (borderWidth * 2);
-
+    for (let index of numbers) {
+        if (index > value) {
+            result.push(index);
+        }
+    }
+    return result;
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); 
-
-console.log(getElementWidth("60px", "12px", "8.5px")); 
-
-console.log(getElementWidth("200px", "0px", "0px"));
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
